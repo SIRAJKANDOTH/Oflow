a1d653974fc747c4a45acde64d5127c915b14f26
sirajddc <siraj@ddc.dev>
2024-05-13 23:57:45 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+// Import necessary modules
+const express = require('express');
+const app = express();
+
+// Define a route for the root URL ("/")
+app.get('/', (req, res) => {
+  res.send('Hello, World!'); // Respond with "Hello, World!"
+});
+
+// Start the server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
